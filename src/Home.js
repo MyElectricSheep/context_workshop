f28ef0c84adeecf1cae560b90e95b5aba3c9ad00
Ben Paget <ben.paget@live.fr>
2020-06-28 09:46:56 +0200

    Initializing Context Workshop

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Welcome from "./Welcome"
 
 const Home = (props) => {
-  return <div>Home Page</div>;
+  return (
+      <>
+  <div>
+  Home Page
+  </div>
+  <br />
+  <Welcome />
+  </>
+  );
 }
 
 export default Home;
