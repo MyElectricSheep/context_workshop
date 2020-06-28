87562802fc8981e767298316ec6873cb9b3ca8f7
Ben Paget <ben.paget@live.fr>
2020-06-28 09:32:45 +0200

    Initializing Context Workshop

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Home from "./Home"
+import About from "./About"
 import Admin from "./Admin"
 import { Link, Route, Switch } from "react-router-dom";
 
@@ -11,10 +12,14 @@ const App = () => {
           <Link to="/">Home Page</Link>
         </li>
         <li>
+          <Link to="/about">About Page</Link>
+        </li>
+        <li>
           <Link to="/admin">Admin Page</Link>
         </li>
       </ul>
       <Switch>
+      <Route path="/about" component={About} />
       <Route path="/admin" component={Admin} />
       <Route exact path="/" component={Home} />
       </Switch>
